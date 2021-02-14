b24ec099109c867cabbf78426938909a95d8e320
3327 櫻井 慎也 (ｻｸﾗｲ ｼﾝﾔ) <sakurai@dc-ishida.ishida.co.jp>
2021-02-14 22:49:31 +0900

    メンバー登録機能追加

--- sample.py
+++ sample.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-
+from flask import request
+import psycopg2
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    import psycopg2
+    
 
     # connect postgreSQL
     users = 'postgres' # initial user
@@ -25,4 +26,17 @@ def hello_world():
     
     return render_template("sample.html",members=members)
 
-app.run()
+@app.route("/member", methods=["POST"])
+def add_member():
+    name = request.form["member_name"]
+    dept_num = request.form["department"]
+    users = 'postgres' # initial user
+    dbnames = 'test'
+    passwords = 'postgres'
+    with psycopg2.connect(host="localhost",port=5432,user=users,database=dbnames,password=passwords) as conn:
+        with conn.cursor() as cur:
+            cur.execute('INSERT INTO mst_member (name,number,department) VALUES (%s,%s,%s)', (name,2,dept_num))
+        conn.commit()
+    return "add"
+
+app.run(debug=True)
